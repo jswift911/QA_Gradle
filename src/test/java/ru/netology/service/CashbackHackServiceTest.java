719fdc5bba20daa0da148edd988d4a7bd75b83a0
Alex <jswift888@gmail.com>
2023-05-13 17:17:18 +0300

    junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -5,7 +5,7 @@ public class CashbackHackServiceTest {
     // 1) Тест, когда сумма меньше границы
 
     @org.junit.Test
-    public void testRemainWithAmountLessThanBoundary() {
+    public void testRemainWithAmountLessThanBoundarJunit4() {
         int amount = 500;
         CashbackHackService service = new CashbackHackService();
         int expected = 500;
@@ -13,10 +13,19 @@ public class CashbackHackServiceTest {
         org.junit.Assert.assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    public void testRemainWithAmountLessThanBoundaryJupiter() {
+        int amount = 500;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
     // 2) Тест, когда сумма равна границе
 
     @org.junit.Test
-    public void testRemainWithAmountEqualToBoundary() {
+    public void testRemainWithAmountEqualToBoundaryJunit4() {
         int amount = 1000;
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
@@ -24,14 +33,32 @@ public class CashbackHackServiceTest {
         org.junit.Assert.assertEquals(expected, actual);
     }
 
+    @org.junit.jupiter.api.Test
+    public void testRemainWithAmountEqualToBoundaryJupiter() {
+        int amount = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
+    }
+
     // 3) Тест, когда сумма больше границы
 
     @org.junit.Test
-    public void testRemainWithAmountGreaterThanBoundary() {
+    public void testRemainWithAmountGreaterThanBoundaryJunit4() {
         int amount = 1500;
         CashbackHackService service = new CashbackHackService();
         int expected = 500;
         int actual = service.remain(amount);
         org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainWithAmountGreaterThanBoundaryJupiter() {
+        int amount = 1500;
+        CashbackHackService service = new CashbackHackService();
+        int expected = 500;
+        int actual = service.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 }
